f7e22cbe5bf321707cece8918ac97c1226e0e566
Jagan-97 <jagan.psaran38@gmail.com>
2022-12-23 09:30:43 +0530

    added Increment Pattern Logic

---
+++ src/IncrementPattern.java
@@ -0,0 +1,11 @@
+public class IncrementPattern{
+    public static void main(String args[]) {
+        for (int i = 3; i <= 6; i++) {
+            for (int j = 3; j <= i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+
